0a530fd1b60d85b9152c07ec58b90bead8840fbf
Diego Farias Castro <dfc_2014@hotmail.com>
2021-03-05 04:20:20 -0600

    Interfaz principal completa

--- ViewAuxiliar.py
+++ ViewAuxiliar.py
@@ -1,3 +1,5 @@
+from PIL import Image, ImageDraw
+import numpy as np
 
 # La clase ofrece metodos auxiliares estaticos 
 # para construir interfaces
@@ -7,3 +9,23 @@ class ViewAuxiliar:
     def obtenerColor(R,G,B):
         return '#%02x%02x%02x' % (R, G, B)
 
+    @staticmethod
+    def recortarImagenUsuario(ubicacion):
+
+        # Abrir imagen 
+        imagen = Image.open(ubicacion).convert("RGB")
+        dimension1 , dimension2 = imagen.size
+        tamano = min(dimension1, dimension2)
+        npImage=np.array(imagen)[:tamano,:tamano]
+
+        # Crear imagen circular
+        alpha = Image.new('L', (tamano,tamano),0)
+        dibujo = ImageDraw.Draw(alpha)
+        dibujo.pieslice([0,0,tamano,tamano],0,360,fill=255)
+
+        # Convert alpha Image to numpy array
+        npAlpha=np.array(alpha)
+        npImage=np.dstack((npImage,npAlpha))
+
+        # Guardar imagen resultante
+        Image.fromarray(npImage).save('recortada.png')
--- ViewPrincipal.py
+++ ViewPrincipal.py
@@ -1,6 +1,6 @@
 import tkinter as Tkinter
 from ViewAuxiliar import ViewAuxiliar
-from PIL import ImageTk, Image 
+from PIL import ImageTk, Image
 
 # La clase representa la vista principal
 class ViewPrincipal():
@@ -9,8 +9,8 @@ class ViewPrincipal():
     def __init__(self):
 
         # Variables para inicializacion
-        largo = 700
-        alto = 500
+        largo = 550
+        alto = 410
         colorFondo = ViewAuxiliar.obtenerColor(243,242,242)
         colorContraste = ViewAuxiliar.obtenerColor(64,64,64)
         colorTextoPanel = ViewAuxiliar.obtenerColor(220,220,220)
@@ -126,10 +126,132 @@ class ViewPrincipal():
         etiquetaDescripcionConfigurar.pack()
         etiquetaDescripcionConfigurar.place(x=180, y=70, height=20, width=75)
 
+        # Etiqueta descriptiva de la labor en la ventana
+        etiquetaDescripcionVentana= Tkinter.Label( 
+            self.__canvas, 
+            text='Seleccionar un usuario para acceder',
+            bg=colorFondo,
+            fg=colorContraste,
+            font="SegoeUI 13 normal"
+        ) # End label
+        etiquetaDescripcionVentana.pack()
+        etiquetaDescripcionVentana.place(x=0, y=110, height=20, width=largo)
+
+        # Recorte temporal
+        ViewAuxiliar.recortarImagenUsuario("assets/ViewPrincipal/imagenUsuarioDefault.jpg")
+
+        # Imagen de usuario
+        imagenUsuario = Image.open("recortada.png")   # Reemplazar con BD
+        imagenUsuario = imagenUsuario.resize((150,150))
+        renderUsuario = ImageTk.PhotoImage(imagenUsuario)
+
+        etiquetaImagenUsuario = Tkinter.Label( 
+            self.__canvas, 
+            bg=colorFondo,
+            image=renderUsuario,
+        ) # End label
+
+        etiquetaImagenUsuario.pack()
+        etiquetaImagenUsuario.place(x=0, y=138, height=150, width=largo)
+
+        # Imagen de moverse a la derecha
+        imagenDerecha = Image.open("assets/ViewPrincipal/moverseDerecha.png")
+        renderDerecha = ImageTk.PhotoImage(imagenDerecha)
+
+        etiquetaImagenDerecha = Tkinter.Label( 
+            self.__canvas, 
+            bg=colorFondo,
+            image=renderDerecha,
+            cursor='hand2'
+        ) # End label
+
+        etiquetaImagenDerecha.pack()
+        etiquetaImagenDerecha.place(x=370, y=178, height=70, width=70)
+
+        # Imagen de moverse a la izquierda
+        imagenIzquierda = Image.open("assets/ViewPrincipal/moverseIzquierda.png")
+        renderIzquierda = ImageTk.PhotoImage(imagenIzquierda)
+
+        etiquetaImagenIzquierda = Tkinter.Label( 
+            self.__canvas, 
+            bg=colorFondo,
+            image=renderIzquierda,
+            cursor='hand2'
+        ) # End label
+
+        etiquetaImagenIzquierda.pack()
+        etiquetaImagenIzquierda.place(x=110, y=178, height=70, width=70)
+
+        # Etiqueta con el nombre del usuario
+        etiquetaNombreUsuario= Tkinter.Label( 
+            self.__canvas, 
+            text='Nombre del usuario',
+            bg=colorFondo,
+            fg=ViewAuxiliar.obtenerColor(64,64,64),
+            font="SegoeUI 16 normal"
+        ) # End label
+        etiquetaNombreUsuario.pack()
+        etiquetaNombreUsuario.place(x=0, y=300, height=20, width=largo)
+
+        # Etiqueta con el nombre del usuario
+        etiquetaNombreUsuario= Tkinter.Label( 
+            self.__canvas, 
+            text='Registrado el 01 de enero de 2021',
+            bg=colorFondo,
+            fg=ViewAuxiliar.obtenerColor(94,94,94),
+            font="SegoeUI 12 normal"
+        ) # End label
+        etiquetaNombreUsuario.pack()
+        etiquetaNombreUsuario.place(x=0, y=320, height=20, width=largo)
+
+        # Boton de escaneo EEG
+        imagenEscaneoEEG = Image.open("assets/ViewPrincipal/escaneoEEG.png")
+        renderEscaneoEEG  = ImageTk.PhotoImage(imagenEscaneoEEG)
+        botonEscaneoEEG = Tkinter.Button( 
+            self.__canvas, 
+            fg=colorContraste, 
+            text ="Usar escaneo EEG", 
+            font="SegoeUI 11 normal",
+            highlightbackground=colorFondo,
+            image=renderEscaneoEEG,
+            compound = Tkinter.LEFT,
+            cursor='hand2'
+        ) # End button
+
+        botonEscaneoEEG.pack()
+        botonEscaneoEEG.place(x=135, y=345, height=28, width=135)
+
+        # Boton de contrasena
+        imagenContrasena = Image.open("assets/ViewPrincipal/contrasena.png")
+        renderContrasena  = ImageTk.PhotoImage(imagenContrasena)
+        botonContrasena = Tkinter.Button( 
+            self.__canvas, 
+            fg=colorContraste, 
+            text ="Usar contraseña", 
+            font="SegoeUI 11 normal",
+            highlightbackground=colorFondo,
+            image=renderContrasena,
+            compound = Tkinter.LEFT,
+            cursor='hand2'
+        ) # End button
+
+        botonContrasena.pack()
+        botonContrasena.place(x=280, y=345, height=28, width=135)
+
+        # Etiqueta del programa
+        etiquetaPrograma= Tkinter.Label( 
+            self.__canvas, 
+            text='Programa de honores. Universidad de las Américas Puebla',
+            bg=colorFondo,
+            fg=ViewAuxiliar.obtenerColor(134,134,134),
+            font="SegoeUI 9 italic",
+            anchor='w'
+        ) # End label
+        etiquetaPrograma.pack()
+        etiquetaPrograma.place(x=10, y=390, height=20, width=largo)
+
         # Ciclo principal
         self.__ventana.mainloop()
 
-
 # Llamada al metodo principal
 interfaz = ViewPrincipal()
-interfaz.construir()

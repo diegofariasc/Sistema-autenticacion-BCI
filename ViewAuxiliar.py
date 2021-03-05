8f7d11ae2e9213a050b7a31544522ef96394384d
Diego Farias Castro <dfc_2014@hotmail.com>
2021-03-05 01:19:07 -0600

    Funcion de generacion de colores desde RGB

---
+++ ViewAuxiliar.py
@@ -0,0 +1,9 @@
+
+# La clase ofrece metodos auxiliares estaticos 
+# para construir interfaces
+class ViewAuxiliar:
+
+    @staticmethod
+    def obtenerColor(R,G,B):
+        return '#%02x%02x%02x' % (R, G, B)
+

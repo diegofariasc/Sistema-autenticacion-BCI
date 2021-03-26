78928a981be784c8cbc0ee628277c8574541c8d2
Diego Farias Castro <dfc_2014@hotmail.com>
2021-03-26 10:54:22 -0600

    En Main se relaciona ahora con un model

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 from ViewPrincipal import ViewPrincipal
 from ControllerPrincipal import ControllerPrincipal
+from Model import Model
 
 def main():
 
     # Construir interfaz principal
     viewPrincipal = ViewPrincipal()
 
-    # Relacionar interfaz principal con el controller e inicializarla
-    controllerViewPrincipal = ControllerPrincipal( viewPrincipal )
+    # Construir model
+    model = Model()
+
+    # Relacionar interfaz principal con el controller y el model e inicializarla
+    controllerViewPrincipal = ControllerPrincipal( viewPrincipal,model )
     controllerViewPrincipal.inicializarView()
 
 main()
